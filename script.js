784992b3e000d80649a34f6ac7aa31929f778ad9
artem01123 <artem.kolenchenko011@gmail.com>
2025-02-12 20:17:10 +0200

    еглкен

---
+++ script.js
@@ -0,0 +1,15 @@
+let isActive = false;
+
+function toggleMenu() {
+    let image = document.getElementById("mainImage");
+    let buttonBox = document.getElementById("buttonBox");
+
+    if (!isActive) {
+        image.src = "Union.png"; // Меняем картинку
+    } else {
+        image.src = "mobile_menu.png"; // Возвращаем старую картинку
+    }
+
+    document.querySelector(".container").classList.toggle("active");
+    isActive = !isActive;
+}
